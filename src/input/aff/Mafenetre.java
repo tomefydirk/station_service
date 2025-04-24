415f22e38cee75b5d96d34cee354b3caea92d185
tomefydirk <tomefydirk@yahoo.com>
2025-04-24 20:09:54 +0300

    test table

--- src/input/aff/Mafenetre.java
+++ src/input/aff/Mafenetre.java
@@ -1,11 +1,27 @@
 package aff;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.*;
 public class Mafenetre extends JFrame{ 
+        void add_table(){
+            String[][] donnee={
+                {"1","Gerard","bleu"},
+                {"2","Claude","bleu"}
+            };
+            String[] colone={
+                "id","nom","couleur"
+            };
+            JTable tableau=new JTable(donnee,colone);
+            JScrollPane sp=new JScrollPane(tableau);
+            add(sp);
+        }
+        
         public Mafenetre(){
             super("Station service");
+            this.setLayout(new GridLayout(0, 2));
             add(new Formulaire());
+            add_table();
             this.setLocationRelativeTo(null);
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setMinimumSize(new Dimension(1000,1000));
