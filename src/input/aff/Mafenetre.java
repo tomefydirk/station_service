8989c1f0443025cb0badaa59ce661f829195fb70
tomefydirk <tomefydirk@yahoo.com>
2025-04-25 12:13:29 +0300

    ajust img

--- src/input/aff/Mafenetre.java
+++ src/input/aff/Mafenetre.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 public class Mafenetre extends JFrame{ 
   
         void make_image(){
@@ -13,14 +14,14 @@ public class Mafenetre extends JFrame{
 
           
             JLabel label = new JLabel(imageIcon);
-            p.setBackground(new Color(0,0, 0));
             p.add(label);
-         
             add(p);
         }
         public Mafenetre(){
             super("Station service");
+            
             this.setLayout(new GridLayout(0, 2));
+            //setBackground(new Color(59,59, 59,10));
             add(new Formulaire());
             make_image();
             this.setLocationRelativeTo(null);
