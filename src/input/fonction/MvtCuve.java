17f6744bb3ae67f115f717e0c0c28498a70c4e7e
tomefydirk <tomefydirk@yahoo.com>
2025-04-29 11:43:55 +0300

    from_row done

--- src/input/fonction/MvtCuve.java
+++ src/input/fonction/MvtCuve.java
@@ -1,6 +1,10 @@
 package fonction;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import lecture.FLoader;
 
 public class MvtCuve {
     Date Mvt;
@@ -31,4 +35,25 @@ public class MvtCuve {
     public void setC(Cuve c) {
         this.c = c;
     }  
+    public static MvtCuve from_row(String[] row,Station s){
+        MvtCuve retour=new MvtCuve();
+        retour.Mvt=new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            Date date = formatter.parse(row[1].trim());
+            System.out.println("Date parsée : " + date);
+        } catch (ParseException e) {
+            System.out.println("Erreur de parsing : " + e.getMessage());
+        }
+
+        retour.c=s.get_cuve(row[0]);
+        retour.entree=Integer.parseInt(row[2]);
+        retour.entree=Integer.parseInt(row[3]);
+        return retour;
+    }
+  /*  public static Mvt_cuve[] from_donnee(String input){
+        String[][] donne=FLoader.get_all_data(input);
+        
+    } */
 }
